084599fe353be4f1e6dc62e32d95cddc5fc1f913
Mehmet Sait Işık <saitisik63@gmail.com>
2022-08-26 21:40:19 +0300

    modal ve modalSlice yapısı kurgulandı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
-import { store } from "./store";
 import { useEffect } from "react";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 function App() {
   //useefect ile spette her degisiklik oldugunda calculate fonksiyonu tetiklenecek ve sayılar güncellenecek
-  const {cartItems}=useSelector((store)=>store.cart);
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(calculateTotals());
-  },[cartItems])
+  }, [cartItems]);
 
   return (
     <main>
-    <Navbar />
-    <CartContainer/>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
     </main>
-
-  )
+  );
 }
 export default App;
