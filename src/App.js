28ba74d6bcd7e6f8a7028e8a703a59be3dee5fb5
Mehmet Sait Işık <saitisik63@gmail.com>
2022-08-27 23:37:35 +0300

    create async thunk added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 function App() {
   //useefect ile spette her degisiklik oldugunda calculate fonksiyonu tetiklenecek ve sayılar güncellenecek
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems,isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+  
+  if(isLoading){
+    return(
+      <div className="loading">
+        <h1>loading...</h1>
+      </div>
+    )
+  }
 
   return (
     <main>
